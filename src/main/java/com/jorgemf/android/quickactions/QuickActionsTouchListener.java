eb08b27f649f6566fecbc1b4b208fd2c9e1da08c
yilun <allan1st@gmail.com>
2015-10-20 11:29:36 +0100

    Fix onDown issue

--- src/main/java/com/jorgemf/android/quickactions/QuickActionsTouchListener.java
+++ src/main/java/com/jorgemf/android/quickactions/QuickActionsTouchListener.java
@@ -8,64 +8,64 @@ import android.view.View.OnTouchListener;
 
 public class QuickActionsTouchListener implements OnTouchListener {
 
-	private GestureDetector gesturesDetector;
+    private GestureDetector gesturesDetector;
 
-	private QuickActionsMenu quickActionsMenu;
+    private QuickActionsMenu quickActionsMenu;
 
-	private Object tag;
+    private Object tag;
 
-	private View view;
+    private View view;
 
-	public QuickActionsTouchListener(Context context, QuickActionsMenu quickActionsMenu) {
-		this.quickActionsMenu = quickActionsMenu;
-		gesturesDetector = new GestureDetector(context, new GestureDetector.OnGestureListener() {
+    public QuickActionsTouchListener(Context context, QuickActionsMenu quickActionsMenu) {
+        this.quickActionsMenu = quickActionsMenu;
+        gesturesDetector = new GestureDetector(context, new GestureDetector.OnGestureListener() {
 
-			@Override
-			public boolean onDown(MotionEvent e) {
-				return false;
-			}
+            @Override
+            public boolean onDown(MotionEvent e) {
+                return true;
+            }
 
-			@Override
-			public void onShowPress(MotionEvent e) {
+            @Override
+            public void onShowPress(MotionEvent e) {
 
-			}
+            }
 
-			@Override
-			public boolean onSingleTapUp(MotionEvent e) {
-				if (QuickActionsTouchListener.this.quickActionsMenu.getVisibility() != View.VISIBLE) {
-					view.callOnClick();
-					return true;
-				}
-				return false;
-			}
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                if (QuickActionsTouchListener.this.quickActionsMenu.getVisibility() != View.VISIBLE) {
+                    view.callOnClick();
+                    return true;
+                }
+                return false;
+            }
 
-			@Override
-			public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-				return false;
-			}
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                return false;
+            }
 
-			@Override
-			public void onLongPress(MotionEvent e) {
-				view.setPressed(false);
-				QuickActionsMenu quickActionsMenu = QuickActionsTouchListener.this.quickActionsMenu;
-				quickActionsMenu.show(e.getRawX(), e.getRawY(), tag);
-			}
+            @Override
+            public void onLongPress(MotionEvent e) {
+                view.setPressed(false);
+                QuickActionsMenu quickActionsMenu = QuickActionsTouchListener.this.quickActionsMenu;
+                quickActionsMenu.show(e.getRawX(), e.getRawY(), tag);
+            }
 
-			@Override
-			public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-				return false;
-			}
-		});
-	}
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                return false;
+            }
+        });
+    }
 
-	@Override
-	public boolean onTouch(View v, MotionEvent event) {
-		if (quickActionsMenu.getVisibility() != View.VISIBLE) {
-			view = v;
-			tag = v.getTag();
-			return gesturesDetector.onTouchEvent(event);
-		}
-		return false;
-	}
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if (quickActionsMenu.getVisibility() != View.VISIBLE) {
+            view = v;
+            tag = v.getTag();
+            return gesturesDetector.onTouchEvent(event);
+        }
+        return false;
+    }
 
 }
